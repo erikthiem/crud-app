49fb60bfdd99c971f689118033b0e3e1bad05baf
Erik Thiem <erikthiem@gmail.com>
2016-12-03 15:54:44 -0600

    Added a route to create a new customer record

--- backend/routes/customer.rb
+++ backend/routes/customer.rb
@@ -4,3 +4,21 @@ get '/customers' do
     content_type :json
     return customers.to_json
 end
+
+post '/customers' do
+    company = params[:company]
+    address = params[:address]
+    city = params[:city]
+	state = params[:state]
+	zip = params[:zip]
+
+    customer = Customer.create(:company => company, :address => address, :city => city, :state => state, :zip => zip)
+
+    saveSuccessful = customer.save
+
+    if saveSuccessful then
+        response.status = 201
+    else
+        response.status = 400
+    end
+end
