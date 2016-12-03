f61b0a91de3ac8520ddd3f38a35bb6b2dc873968
Erik Thiem <erikthiem@gmail.com>
2016-12-02 19:42:41 -0600

    splitup app.rb into sub files

--- backend/app.rb
+++ backend/app.rb
@@ -1,19 +1,4 @@
 require 'sinatra'
-require 'data_mapper'
 
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/crud.db")
-require_relative 'models/user.rb'
-require_relative 'models/customer.rb'
-require_relative 'models/project.rb'
-require_relative 'models/task.rb'
-require_relative 'models/task_entry.rb'
-DataMapper.finalize
-User.auto_upgrade!
-Customer.auto_upgrade!
-Project.auto_upgrade!
-Task.auto_upgrade!
-Task_entry.auto_upgrade!
-
-get '/' do
-    "Hello World"
-end
+require_relative 'models.rb'
+require_relative 'routes.rb'
