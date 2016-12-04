0d2864bcc8fcb03e90c344f963863ebf1c6f80d2
Erik Thiem <erikthiem@gmail.com>
2016-12-03 18:07:30 -0600

    Added route to create new task_entry

--- backend/routes/task_entry.rb
+++ backend/routes/task_entry.rb
@@ -4,3 +4,18 @@ get '/task_entries' do
     content_type :json
     return task_entries.to_json
 end
+
+post '/task_entries' do
+    note = params[:note]
+    task_id = params[:task_id]
+
+    task_entry = TaskEntry.create(:note => note, :task_id => task_id)
+
+    saveSuccessful = task_entry.save
+
+    if saveSuccessful then
+        response.status = 201
+    else
+        response.status = 400
+    end
+end
