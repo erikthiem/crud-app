d447d1416d68e7ef9b543bd1947edd0ccf73046a
Erik Thiem <erikthiem@gmail.com>
2016-12-04 11:07:18 -0600

    Added view all users to frontend

--- frontend/js/vueApp.js
+++ frontend/js/vueApp.js
@@ -6,3 +6,35 @@ var vueApp = new Vue({
     }
 
 })
+
+var userSection = new Vue({
+
+    el: '#userSection',
+    data: {
+        users: []
+    },
+    mounted: function() {
+        var self = this;
+        self.getUsers();
+    },
+    methods: {
+        getUsers: function() {
+            var self = this;
+            $.ajax({
+                url: "http://localhost:4567/users",
+                contentType: "application/json",
+                data: self.users,
+                dataType: "text json",
+                method: "GET",
+                success: function(data) {
+                    self.users = data;
+                },
+                error: function(error) {
+                    console.log(JSON.stringify(error));
+                }
+            });
+
+        }
+    }
+
+})
