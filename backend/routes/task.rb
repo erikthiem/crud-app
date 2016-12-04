9bed026b34dd30e08f24b3bd1917dd9497401384
Erik Thiem <erikthiem@gmail.com>
2016-12-03 18:00:47 -0600

    Added create route for tasks

--- backend/routes/task.rb
+++ backend/routes/task.rb
@@ -4,3 +4,19 @@ get '/tasks' do
     content_type :json
     return tasks.to_json
 end
+
+post '/tasks' do
+    task_name = params[:task_name]
+    project_id = params[:project_id]
+    user_id = params[:user_id]
+
+    task = Task.create(:task_name => task_name, :project_id => project_id, :user_id => user_id)
+
+    saveSuccessful = task.save
+
+    if saveSuccessful then
+        response.status = 201
+    else
+        response.status = 400
+    end
+end
