2cdf56f0ed4d16dcd6969a6e574cb78c01e3e216
Erik Thiem <erikthiem@gmail.com>
2016-12-03 23:15:29 -0600

    Added more routes for task entries

--- backend/routes/task_entry.rb
+++ backend/routes/task_entry.rb
@@ -1,3 +1,4 @@
+# Get all task entries
 get '/task_entries' do
     task_entries = TaskEntry.all
 
@@ -5,17 +6,47 @@ get '/task_entries' do
     return task_entries.to_json
 end
 
+
+# Get a specific task entry
+get '/task_entry/:id' do
+    id = params[:id]
+
+    task_entry = TaskEntry.get(id)
+
+	get_item(task_entry)
+end
+
+
+# Create a new task entry
 post '/task_entries' do
     note = params[:note]
     task_id = params[:task_id]
 
     task_entry = TaskEntry.create(:note => note, :task_id => task_id)
 
-    saveSuccessful = task_entry.save
+	create_item(task_entry)
+end
 
-    if saveSuccessful then
-        response.status = 201
-    else
-        response.status = 400
-    end
+
+# Modify a task entry
+put '/task_entry/:id' do
+    id = params[:id]
+    note = params[:note]
+    task_id = params[:task_id]
+
+    updated_parameters = {:note => note, :task_id => task_id}
+
+    task_entry = TaskEntry.get(id)
+
+    update_item(task_entry, updated_parameters)
+end
+
+
+# Delete a task entry
+delete '/task_entry/:id' do
+    id = params[:id]
+
+    task_entry = TaskEntry.get(id)
+
+    delete_item(task_entry)
 end
