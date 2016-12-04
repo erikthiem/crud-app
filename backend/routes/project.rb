d440b5d5a5f5bfde2934f61864538919d8a2b830
Erik Thiem <erikthiem@gmail.com>
2016-12-03 22:40:16 -0600

    Added more routes for projects

--- backend/routes/project.rb
+++ backend/routes/project.rb
@@ -1,3 +1,4 @@
+# Get all projects
 get '/projects' do
     projects = Project.all
 
@@ -5,18 +6,46 @@ get '/projects' do
     return projects.to_json
 end
 
+# Get a specific project
+get '/project/:id' do
+    id = params[:id]
+
+    project = Project.get(id)
+
+	get_item(project)
+end
+
+
+# Create a new project
 post '/projects' do
     project_name = params[:project_name]
     customer_id = params[:customer_id]
 
     project = Project.create(:project_name => project_name, :customer_id => customer_id)
 
-    saveSuccessful = project.save
-
-    if saveSuccessful then
-        response.status = 201
-    else
-        response.status = 400
-    end
+    create_item(project)
 end
 
+
+# Modify a project
+put '/project/:id' do
+    id = params[:id]
+    project_name = params[:project_name]
+    customer_id = params[:customer_id]
+
+    updated_parameters = {:project_name => project_name, :customer_id => customer_id}
+
+    project = Project.get(id)
+
+    update_item(project, updated_parameters)
+end
+
+
+# Delete a project
+delete '/project/:id' do
+    id = params[:id]
+
+    project = Project.get(id)
+
+    delete_item(project)
+end
