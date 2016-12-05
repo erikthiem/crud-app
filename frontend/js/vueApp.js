c1f4b3b56819965c687c3f178f4281431e42e44d
Erik Thiem <erikthiem@gmail.com>
2016-12-04 19:55:38 -0600

    Display all entities on frontend

--- frontend/js/vueApp.js
+++ frontend/js/vueApp.js
@@ -1,28 +1,32 @@
+var apiBaseUrl = "http://localhost:4567"
+
 var vueApp = new Vue({
     
     el: '#vueApp',
     data: {
-        message: 'CRUD App'
-    }
-
-})
-
-var userSection = new Vue({
-
-    el: '#userSection',
-    data: {
-        users: []
+        users: [],
+        customers: [],
+        projects: [],
+        tasks: [],
+        task_entries: []
     },
+
     mounted: function() {
+
         var self = this;
         self.getUsers();
+        self.getCustomers();
+        self.getProjects();
+        self.getTasks();
+        self.getTaskEntries();
     },
+
     methods: {
+
         getUsers: function() {
             var self = this;
             $.ajax({
-                url: "http://localhost:4567/users",
-                contentType: "application/json",
+                url: apiBaseUrl + "/users", contentType: "application/json",
                 dataType: "json",
                 method: "GET",
                 success: function(data) {
@@ -32,25 +36,12 @@ var userSection = new Vue({
                     console.log(JSON.stringify(error));
                 }
             });
-        }
-    }
-})
+        },
 
-var customerSection = new Vue({
-
-    el: '#customerSection',
-    data: {
-        customers: []
-    },
-    mounted: function() {
-        var self = this;
-        self.getCustomers();
-    },
-    methods: {
         getCustomers: function() {
             var self = this;
             $.ajax({
-                url: "http://localhost:4567/customers",
+                url: apiBaseUrl + "/customers",
                 contentType: "application/json",
                 dataType: "json",
                 method: "GET",
@@ -61,6 +52,56 @@ var customerSection = new Vue({
                     console.log(JSON.stringify(error));
                 }
             });
+        },
+
+        getProjects: function() {
+            var self = this;
+            $.ajax({
+                url: apiBaseUrl + "/projects",
+                contentType: "application/json",
+                dataType: "json",
+                method: "GET",
+                success: function(data) {
+                    self.projects = data;
+                },
+                error: function(error) {
+                    console.log(JSON.stringify(error));
+                }
+            });
+        },
+
+        getTasks: function() {
+            var self = this;
+            $.ajax({
+                url: apiBaseUrl + "/tasks",
+                contentType: "application/json",
+                dataType: "json",
+                method: "GET",
+                success: function(data) {
+                    self.tasks = data;
+                },
+                error: function(error) {
+                    console.log(JSON.stringify(error));
+                }
+            });
+        },
+
+        getTaskEntries: function() {
+            var self = this;
+            $.ajax({
+                url: apiBaseUrl + "/task_entries",
+                contentType: "application/json",
+                dataType: "json",
+                method: "GET",
+                success: function(data) {
+                    self.task_entries = data;
+                },
+                error: function(error) {
+                    console.log(JSON.stringify(error));
+                }
+            });
         }
+
+
     }
 })
